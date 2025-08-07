7f2f8361ae2c8a92ab3ec56248de5ecaa0f2e576
troy0915 <johntroydolom7@gmail.com>
2025-08-07 15:09:37 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_3Username_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> usernames = new List<string>();
+            Console.WriteLine("Enter 5 usernames:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Username {i + 1}: ");
+                string username = Console.ReadLine();
+                usernames.Add(username);
+            }
+            Console.Write("Enter a character to search for in the usernames: ");
+            char searchChar = Console.ReadKey().KeyChar;
+            Console.WriteLine(); // Move to the next line
+            Console.WriteLine("\nUser names containing the character '{0}':", searchChar);
+            bool found = false;
+
+            foreach (var username in usernames)
+            {
+                if (username.ToLower().Contains(searchChar.ToString().ToLower()))
+                {
+                    Console.WriteLine(username);
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("No usernames contain the character '{0}'.", searchChar);
+            }
+        }
+    }
+}
+    
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Activity 3Username Filter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Activity 3Username Filter")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b7f7903-4980-45a2-9a7c-30025b39a7ce")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
